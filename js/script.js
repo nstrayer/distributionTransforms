5137994621c0f14441a69ff5c15d75f9d41a9ec3
Nick Strayer <nstrayer@uvm.edu>
2015-09-18 14:06:07 -0500

    Added the switching

--- js/script.js
+++ js/script.js
@@ -7,49 +7,73 @@ d3.selection.prototype.moveToFront = function() {
 
 var width = parseInt(d3.select("#viz").style("width").slice(0, -2)),
     height = $(window).height() - 120;
-//
 
-d3.csv("data/normalData.csv", function(orig_data){
-    orig_data.forEach(function(d){
-        d.x = +d.x
-        d.y = +d.y
+
+// d3.csv("data/normalData.csv", function(orig_data){
+normal_descrips = ["Start with some points drawn from the normal distribution.",
+                  "Drop those points (transform them) onto the CDF of the normal...",
+                  "...and end up with uniformly distributed points." ]
+
+chi2_descrips = ["Start with some points drawn from the normal distribution.",
+                "Drop those points (transform them) onto the equation y = x^2...",
+                "...and end up with chi_squared distributed points." ]
+
+function runViz(fileName, descrips){
+    d3.csv("data/" + fileName, function(orig_data){
+        orig_data.forEach(function(d){
+            d.x = +d.x
+            d.y = +d.y
+        })
+
+        //set up data with the different states of the visualization.
+
+        var state1 = JSON.parse(JSON.stringify(orig_data)); //it's hard to clone an object!
+        state1.forEach(function(d){ d.y = 1 })
+
+        //state 2 is just the original data
+
+        var state3 = JSON.parse(JSON.stringify(orig_data)); //it's hard to clone an object!
+        state3.forEach(function(d){ d.x = 1 })
+
+        var states = [{"desc": descrips[0], "data": state1},
+                      {"desc": descrips[1], "data":orig_data},
+                      {"desc": descrips[2], "data":state3}
+                     ]
+
+        var updatableChart = scatter()
+            .width(width).height(height)
+            .data(state1)
+            .animationDuration(800);
+
+        d3.select('#viz')
+          .call(updatableChart);
+
+        var i = 1;
+
+        function nextButton(){
+            updatableChart.data(states[i].data);
+            d3.select("#text").text(states[i].desc)
+            i = (i+1) % 3 ;
+        }
+        d3.select("#text").text(states[0].desc)
+        d3.select("#next").on("click",nextButton)
     })
+}
 
-    //set up data with the different states of the visualization.
+//initialize the viz
+runViz("chiSquaredVals.csv", chi2_descrips)
 
-    var state1 = JSON.parse(JSON.stringify(orig_data)); //it's hard to clone an object!
-    state1.forEach(function(d){ d.y = 1 })
-
-    //state 2 is just the original data
-
-    var state3 = JSON.parse(JSON.stringify(orig_data)); //it's hard to clone an object!
-    state3.forEach(function(d){ d.x = 1 })
-
-    var states = [{"desc": "Start with some points drawn from the normal distribution.", "data": state1},
-                  {"desc": "Drop those points (transform them) onto the CDF of the normal...", "data":orig_data},
-                  {"desc": "...and end up with uniformly distributed points.", "data":state3}
-              ]
-
-    var updatableChart = scatter()
-        .width(width).height(height)
-        .data(state1)
-        .animationDuration(800);
-
-    d3.select('#viz')
-      .call(updatableChart);
-
-    var i = 1;
-
-    function nextButton(){
-        updatableChart.data(states[i].data);
-        d3.select("#description").text(states[i].desc)
-        // if(i == 3){
-        //     d3.select("#next").text("Start over.")
-        // }else if(i == 0){
-        //     d3.select("#next").text("Next.")
-        // }
-        i = (i+1) % 3 ;
+//allow the user to change which transform they see.
+d3.selectAll(".typeChoose").on("click", function(d){
+    var selection = d3.select(this)
+    console.log(selection.attr("fileName"))
+    d3.selectAll(".typeChoose").classed("selected", false)
+    selection.classed("selected", true)
+    d3.select("svg").remove()
+    if(selection.attr("id") == "normal"){ //grab the correct descrips for the viz
+        var descrips = normal_descrips
+    } else {
+        var descrips = chi2_descrips
     }
-    d3.select("#next").on("click",nextButton)
-
+    runViz(selection.attr("fileName"), descrips)
 })
